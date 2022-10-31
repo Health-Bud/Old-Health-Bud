eb4bf90897c7371003c0a4e25ff2559d0c3e61b2
jwong1209 <jwong1209qaz@gmail.com>
2022-10-30 18:52:49 -0700

    Added way to change between fragments

--- app/src/main/java/com/example/health_bud/MainActivity.kt
+++ app/src/main/java/com/example/health_bud/MainActivity.kt
@@ -6,23 +6,41 @@ import android.widget.CalendarView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val calendarView = findViewById<CalendarView>(R.id.calendarView2)
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val logFragment: Fragment = LogFragment()
 
-        /*
-        calendarView.setOnDateChangeListener { calendarView, year, month, day ->
-            Toast.makeText(this, year.toString() + (month+1).toString() + day.toString(), Toast.LENGTH_SHORT).show()
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.list_frame_layout, logFragment)
-            fragmentTransaction.commit()
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define fragments here
+        val DashboardFragment: Fragment = DashboardFragment()
+        val LogFragment: Fragment = LogFragment()
+        val CalendarFragment: Fragment = CalendarFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_dashboard -> fragment = DashboardFragment
+                R.id.nav_log -> fragment = LogFragment
+                R.id.nav_calendar -> fragment = CalendarFragment
+            }
+            replaceFragment(fragment)
+            true
         }
-        */
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_dashboard
+    }
+
+    private fun replaceFragment(xFragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.list_frame_layout, xFragment)
+        fragmentTransaction.commit()
     }
 }
